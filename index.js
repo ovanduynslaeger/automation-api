00863d568b4f71b4465f9c22dadab4ff90dcbb00
Olivier Vanduynslaeger <o.vanduynslaeger@gmail.com>
2015-03-07 16:41:08 +0000

    Errors management

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 //              Routes                   //
 ///////////////////////////////////////////
 
-var api_dio = require('apis/api_dio')
-var api_rf433 = require('apis/api_rf433')
+var api_dio = require('./apis/api_dio')
+var api_rf433 = require('./apis/api_rf433')
 
 module.exports = function (app) {
-    app.get('/dio/device/:deviceId/command/:command', api_dio.deviceCommand);
+    app.get('/dio/remote/:remoteDio/device/:deviceId/command/:command', api_dio.deviceCommand);
     app.get('/rf433/home/:homeCode/device/:deviceId/command/:command', api_rf433.deviceCommand);
 };
