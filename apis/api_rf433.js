e8147b2ad06cf9e47feff4cbae1f4a08bf1c705d
Olivier Vanduynslaeger <o.vanduynslaeger@gmail.com>
2015-03-09 18:29:01 +0000

    Include rcsiwtch module and create .cpp for dio

--- apis/api_rf433.js
+++ apis/api_rf433.js
@@ -1,5 +1,7 @@
 //var homeCodeRF433 = process.env.HOMECODERF433;
 var exec = require('child_process').exec;
+var rcswitch = require('rcswitch'); // Might throw an error if wiring pi init failed (no root)
+
 
 exports.deviceCommand = function(req, res){
     var homeCode = req.params.homeCode;
@@ -26,18 +28,30 @@ function translateCommand(word){
 
 function runDeviceCommand(homeCode,deviceId,command,callback) {
 
-    var commandCode = translateCommand(command);
-    if (commandCode != null) {
-        var commandLine = 'sudo send-rf433 0 '+homeCode+' '+deviceId+' '+commandCode;
+    //var commandCode = translateCommand(command);
+    //if (commandCode != null) {
+/*        var commandLine = 'sudo send-rf433 0 '+homeCode+' '+deviceId+' '+commandCode;
     
-        console.log(commandLine);
+    console.log(commandLine);
          exec(commandLine, function (error, stdout, stderr) {
               if(error) console.error(error);
               //return null if no error 
               callback(error);
           });
-    } else {
-        callback( new Error("Invalid command " + command));
+*/
+
+    rcswitch.enableTransmit(0); // Use data Pin 0
+    var noDevice = deviceId.charCodeAt(0) - 'A' + 1;
+    console.log("Device id = "+noDevice);
+    switch (command) {
+        case "1":
+            rcswitch.switchOn(homeCode, noDevice); 
+            callback(null);
+        case "0":
+            rcswitch.switchOff(homeCode, noDevice);
+            callback(null);
+        default:
+            callback( new Error("Invalid command " + command));
     }
     
 };
